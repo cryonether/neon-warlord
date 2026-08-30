ca6cef407021d379028137944a5f0ebec97ab9c4
FirePrincess01 <SarahSteinbrecher01@gmx.at>
2026-08-30 11:40:29 +0200

    fixed tests

--- neon-warlord/src/reinforcement_learning.rs
+++ neon-warlord/src/reinforcement_learning.rs
@@ -9,3 +9,16 @@ pub mod neural_network_dfdx;
 #[allow(dead_code)]
 pub mod neural_network_simd;
 pub mod ppo;
+
+
+pub fn _assert_f32_eq(a: f32, b: f32, epsilon: f32) {
+    assert!(
+        (a - b).abs() < epsilon,
+        "expected {a} ≈ {b}, difference = {}",
+        (a - b).abs()
+    );
+}
+
+pub fn _assert_f32_eq_(a: f32, b: f32) {
+    _assert_f32_eq(a, b, 0.1);
+}
--- neon-warlord/src/reinforcement_learning/neural_network_simd/test_neural_network_simd.rs
+++ neon-warlord/src/reinforcement_learning/neural_network_simd/test_neural_network_simd.rs
@@ -1,6 +1,6 @@
 //! Tests for NeuralNetworkSimd
 
-use crate::reinforcement_learning::neural_network::NeuralNetwork;
+use crate::reinforcement_learning::{_assert_f32_eq, _assert_f32_eq_, neural_network::NeuralNetwork};
 
 use super::*;
 
@@ -25,15 +25,15 @@ fn compare() {
     let mut nn_0 = NeuralNetwork::new();
     nn_0.x = [1.0, 2.0].into();
 
-    nn_0.w_0 = [[1.0, 1.0], [1.0, 1.0]].into();
-    nn_0.w_1 = [[1.0, 1.0], [1.0, 1.0]].into();
-    nn_0.w_2 = [[1.0, 1.0], [1.0, 1.0]].into();
-    nn_0.w_3 = [[1.0, 1.0], [1.0, 1.0]].into();
+    nn_0.w_0 = [[1.0, 1.0], [2.0, 2.0]].into();
+    nn_0.w_1 = [[1.0, 1.0], [2.0, 2.0]].into();
+    nn_0.w_2 = [[1.0, 1.0], [2.0, 2.0]].into();
+    nn_0.w_3 = [[1.0, 1.0], [2.0, 2.0]].into();
 
-    nn_0.b_0 = [1.0, 1.0].into();
-    nn_0.b_1 = [1.0, 1.0].into();
-    nn_0.b_2 = [1.0, 1.0].into();
-    nn_0.b_3 = [1.0, 1.0].into();
+    nn_0.b_0 = [1.0, 2.0].into();
+    nn_0.b_1 = [1.0, 2.0].into();
+    nn_0.b_2 = [1.0, 2.0].into();
+    nn_0.b_3 = [1.0, 2.0].into();
 
     nn_0.forward();
     nn_0.backward(0);
@@ -45,52 +45,52 @@ fn compare() {
     ];
 
     nn_1.w[0][0] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
     nn_1.w[0][1] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
 
     nn_1.w[1][0] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
     nn_1.w[1][1] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
 
     nn_1.w[2][0] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
     nn_1.w[2][1] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
 
     nn_1.w_y[0] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
     nn_1.w_y[1] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
 
     nn_1.b[0] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
     nn_1.b[1] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
     nn_1.b[2] = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
 
     nn_1.b_y = [
-        1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+        1.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
     ];
 
     nn_1.forward();
     nn_1.backward(0);
 
-    println!("nn_0: {:}", nn_0);
-    println!("nn_1: {:}", nn_1);
+    // println!("nn_0: {:}", nn_0);
+    // println!("nn_1: {:}", nn_1);
 
     assert_eq!(nn_0.y[0], nn_1.y[0]);
 
@@ -153,22 +153,99 @@ fn compare_dfdx() {
     model.3.weight = dev.ones() * 0.1;
     model.3.bias = dev.ones() * 0.1;
 
-    let mut _grads: Gradients<f32, Cpu> = model.alloc_grads();
+    let mut grads: Gradients<f32, Cpu> = model.alloc_grads();
 
     let x: Tensor<Rank2<1, 16>, f32, Cpu> = dev.tensor([[
         1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
     ]]);
-    let y = model.forward_mut(x.traced(_grads));
+    let y = model.forward_mut(x.traced(grads));
+    let y_vec = y.as_vec();
     // println!("y: {:?}", y.as_vec());
     let loss = y.sum();
 
-    _grads = loss.backward();
+    grads = loss.backward();
 
-    // println!("nn_1: {:}", nn_1);
+    println!("nn_1: {:}", nn_1);
 
     // println!("nn_2:");
-    // print_model(&model);
-    // print_grads(&model, &grads);
+    print_model(&model);
+    print_grads(&model, &grads);
+
+
+    let w_0 = &model.0.0.weight.as_vec();
+    let b_0 = &model.0.0.bias.as_vec();
+
+    let w_1 = &model.1.0.weight.as_vec();
+    let b_1 = &model.1.0.bias.as_vec();
+
+    let w_2 = &model.2.0.weight.as_vec();
+    let b_2 = &model.2.0.bias.as_vec();
+
+    let w_3 = &model.3.weight.as_vec();
+    let b_3 = &model.3.bias.as_vec();
+
+
+
+    let dw_0 = grads.get(&model.0.0.weight).as_vec();
+    let db_0 = grads.get(&model.0.0.bias).as_vec();
+
+    let dw_1 = grads.get(&model.1.0.weight).as_vec();
+    let db_1 = grads.get(&model.1.0.bias).as_vec();
+
+    let dw_2 = grads.get(&model.2.0.weight).as_vec();
+    let db_2 = grads.get(&model.2.0.bias).as_vec();
+
+    let dw_3 = grads.get(&model.3.weight).as_vec();
+    let db_3 = grads.get(&model.3.bias).as_vec();
+
+
+    _assert_f32_eq_(y_vec[0], nn_1.y[0]);
+
+    _assert_f32_eq_(b_0[0], nn_1.b[0][0]);
+    _assert_f32_eq_(b_0[1], nn_1.b[0][1]);
+
+    _assert_f32_eq_(b_1[0], nn_1.b[1][0]);
+    _assert_f32_eq_(b_1[1], nn_1.b[1][1]);
+
+    _assert_f32_eq_(b_2[0], nn_1.b[2][0]);
+    _assert_f32_eq_(b_2[1], nn_1.b[2][1]);
+
+    _assert_f32_eq_(b_3[0], nn_1.b_y[0]);
+    // assert_f32_eq_(b_3[1], nn_1.b_y[1]);
+
+
+    _assert_f32_eq_(db_0[0], nn_1.dy_db[0][0]);
+    _assert_f32_eq_(db_0[1], nn_1.dy_db[0][1]);
+
+    _assert_f32_eq_(db_1[0], nn_1.dy_db[1][0]);
+    _assert_f32_eq_(db_1[1], nn_1.dy_db[1][1]);
+
+    _assert_f32_eq_(db_2[0], nn_1.dy_db[2][0]);
+    _assert_f32_eq_(db_2[1], nn_1.dy_db[2][1]);
+
+    _assert_f32_eq_(db_3[0], nn_1.dy_db_y[0]);
+    // assert_f32_eq_(db_3[1], nn_1.dy_db_y[1]);
+
+    _assert_f32_eq_(dw_0[0], nn_1.dy_dw[0][0][0]);
+    _assert_f32_eq_(dw_0[1], nn_1.dy_dw[0][0][1]);
+    _assert_f32_eq_(dw_0[LANES + 0], nn_1.dy_dw[0][1][0]);
+    _assert_f32_eq_(dw_0[LANES + 1], nn_1.dy_dw[0][1][1]);
+
+    _assert_f32_eq_(dw_1[0], nn_1.dy_dw[1][0][0]);
+    _assert_f32_eq_(dw_1[1], nn_1.dy_dw[1][0][1]);
+    _assert_f32_eq_(dw_1[2], nn_1.dy_dw[1][1][0]);
+    _assert_f32_eq_(dw_1[3], nn_1.dy_dw[1][1][1]);
+
+    _assert_f32_eq_(dw_2[0], nn_1.dy_dw[2][0][0]);
+    _assert_f32_eq_(dw_2[1], nn_1.dy_dw[2][0][1]);
+    _assert_f32_eq_(dw_2[2], nn_1.dy_dw[2][1][0]);
+    _assert_f32_eq_(dw_2[3], nn_1.dy_dw[2][1][1]);
+
+    _assert_f32_eq_(dw_3[0], nn_1.dy_dw_y[0][0]);
+    _assert_f32_eq_(dw_3[1], nn_1.dy_dw_y[0][1]);
+    // assert_f32_eq_(dw_3[2], nn_1.dy_dw_y[1][0]);
+    // assert_f32_eq_(dw_3[3], nn_1.dy_dw_y[1][1]);
+
 }
 
 fn print_model(model: &ModelType) {
