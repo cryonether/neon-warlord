a75ce7c91c13625bd3cd3df6a03d6fd6c173564b
FirePrincess01 <SarahSteinbrecher01@gmx.at>
2026-09-04 10:37:02 +0200

    q-learning a maze works

--- neon-warlord/src/reinforcement_learning/q_learning.rs
+++ neon-warlord/src/reinforcement_learning/q_learning.rs
@@ -11,6 +11,8 @@ pub struct QLearning<const INPUTS: usize, const OUTPUTS: usize> {
     state: HashMap<StateKey<INPUTS>, StateValue<OUTPUTS>>,
     
     steps: Vec<Transition<INPUTS>>,
+
+    epsilon: f32,
 }
 
 impl<const INPUTS: usize, const OUTPUTS: usize> QLearning<INPUTS, OUTPUTS> {
@@ -18,7 +20,8 @@ impl<const INPUTS: usize, const OUTPUTS: usize> QLearning<INPUTS, OUTPUTS> {
     pub fn new() -> Self {
         let state: HashMap<StateKey<INPUTS>, StateValue<OUTPUTS>> = HashMap::new();
         let steps = Vec::new();
-        Self { state, steps }
+        let epsilon = 0.8;
+        Self { state, steps, epsilon }
     }
 
     pub fn choose_action(&mut self, inputs: &[u8; INPUTS]) -> (usize, [f32; OUTPUTS])
@@ -29,44 +32,120 @@ impl<const INPUTS: usize, const OUTPUTS: usize> QLearning<INPUTS, OUTPUTS> {
             None => [0.0; OUTPUTS],
         };
 
-        let mut action = 0;
-        for i in 1..OUTPUTS {
-            if q_values[i] > q_values[action] {
-                action = i;
-            }
-        }
+        let mut action = Self::pick_action(q_values);
 
-        if fastrand::f32() < 0.1 {
+        if fastrand::f32() < self.epsilon {
             action = fastrand::usize(0..OUTPUTS);
         }
 
         (action, q_values)
     }
 
-    pub fn set_reward(&mut self, inputs: &[u8; INPUTS], action: usize, reward: f32) 
+    fn pick_action_probability(mut q_values: [f32; OUTPUTS]) -> usize {
+
+        let min = q_values
+            .iter()
+            .copied()
+            .fold(f32::INFINITY, f32::min);
+
+        // Shift values so the smallest value becomes 0.
+        for value in &mut q_values {
+            *value -= min;
+        }
+
+        let sum: f32 = q_values.iter().sum();
+
+        // All Q-values are equal (or invalid), so choose uniformly.
+        if sum <= 0.0 || !sum.is_finite() {
+            return fastrand::usize(..OUTPUTS);
+        }
+
+        let random = fastrand::f32() * sum;
+        let mut cumulative = 0.0;
+
+        for (i, &value) in q_values.iter().enumerate() {
+            cumulative += value;
+
+            if random < cumulative {
+                return i;
+            }
+        }
+
+        // Floating-point rounding fallback.
+        OUTPUTS - 1
+    }
+
+
+    fn pick_action(q_values: [f32; OUTPUTS]) -> usize {
+
+        let mut q_value_max = f32::NEG_INFINITY;
+        let mut max_index = 0;
+        for (i, &q_value) in q_values.iter().enumerate() {
+            if q_value > q_value_max {
+                q_value_max = q_value;
+                max_index = i;
+            }
+        }
+
+        max_index
+    }
+
+    pub fn set_reward(
+        &mut self, 
+        inputs: [u8; INPUTS], 
+        action: usize, 
+        reward: f32,
+        next_inputs: [u8; INPUTS], 
+    ) 
     {
         self.steps.push(Transition{
-            inputs: *inputs,
+            inputs,
             action,
             reward,
+            inputs_next: next_inputs,
         });
     }
 
     pub fn learn(&mut self) 
     {
-        for step in &self.steps {
-            let state_key = StateKey{inputs: step.inputs};
+        const GAMMA: f32 = 0.9;
+
+        for step in self.steps.iter().rev() {
+            let inputs = step.inputs;
+            let action = step.action;
+            let reward = step.reward;
+            let inputs_next = step.inputs_next;
+
+            let state_key = StateKey{inputs: inputs};
+            let state_key_next = StateKey{inputs: inputs_next};
 
             let mut q_values: [f32; OUTPUTS] = match self.state.get(&state_key) {
                 Some(state_value) => state_value.q_values,
                 None => [0.0; OUTPUTS],
             };
 
-            q_values[step.action] = step.reward;
+            let q_values_next: [f32; OUTPUTS] = match self.state.get(&state_key_next) {
+                Some(state_value) => state_value.q_values,
+                None => [0.0; OUTPUTS],
+            };
+
+            let mut q_value_max_next = f32::NEG_INFINITY;
+            for q_value in q_values_next {
+                if q_value > q_value_max_next {
+                    q_value_max_next = q_value
+                }
+            }
+
+            let reward_2 = reward + GAMMA * (q_value_max_next - reward);
+
+            // modify
+            q_values[action] = reward_2;
             self.state.insert(state_key, StateValue{
                 q_values,
             });
         }
+
+        self.epsilon = f32::max(self.epsilon * 0.95, 0.1); 
     }
 }
 
@@ -74,6 +153,7 @@ pub struct Transition<const INPUTS: usize> {
     pub inputs: [u8; INPUTS],
     pub action: usize,
     pub reward: f32,
+    pub inputs_next: [u8; INPUTS],
 }
 
 #[derive(Hash, Eq, PartialEq)]
--- neon-warlord/src/reinforcement_learning/q_learning/test_maze.rs
+++ neon-warlord/src/reinforcement_learning/q_learning/test_maze.rs
@@ -1,8 +1,43 @@
 //! Learns the path through the maze
 
+use std::{os::unix::thread, time::Duration};
+
 use crate::reinforcement_learning::q_learning::QLearning;
 
-const WALL: usize = 1;
+pub const RESET: &str = "\x1b[0m";
+
+// Standard colors
+pub const BLACK: &str = "\x1b[30m";
+pub const RED: &str = "\x1b[31m";
+pub const GREEN: &str = "\x1b[32m";
+pub const YELLOW: &str = "\x1b[33m";
+pub const BLUE: &str = "\x1b[34m";
+pub const MAGENTA: &str = "\x1b[35m";
+pub const CYAN: &str = "\x1b[36m";
+pub const WHITE: &str = "\x1b[37m";
+
+// Bright colors
+pub const BRIGHT_BLACK: &str = "\x1b[90m";
+pub const BRIGHT_RED: &str = "\x1b[91m";
+pub const BRIGHT_GREEN: &str = "\x1b[92m";
+pub const BRIGHT_YELLOW: &str = "\x1b[93m";
+pub const BRIGHT_BLUE: &str = "\x1b[94m";
+pub const BRIGHT_MAGENTA: &str = "\x1b[95m";
+pub const BRIGHT_CYAN: &str = "\x1b[96m";
+pub const BRIGHT_WHITE: &str = "\x1b[97m";
+
+// Useful 256-color shades
+pub const GRAY: &str = "\x1b[38;5;245m";
+pub const DARK_GRAY: &str = "\x1b[38;5;238m";
+
+pub const ORANGE: &str = "\x1b[38;5;208m";
+pub const RUST: &str = "\x1b[38;5;130m";
+pub const GOLD: &str = "\x1b[38;5;220m";
+
+pub const PINK: &str = "\x1b[38;5;205m";
+pub const PURPLE: &str = "\x1b[38;5;129m";
+pub const TEAL: &str = "\x1b[38;5;30m";
+pub const LIME: &str = "\x1b[38;5;118m";
 
 struct Maze<const W: usize, const H: usize> {
     maze: [[u8; W]; H],
@@ -71,21 +106,31 @@ impl<const W: usize, const H: usize> Maze<W, H>
         self.position = self.start;
     }
 
-    fn get_position<const WH:usize>(&self) -> [u8; WH] 
-    {
-        assert!(WH >= W + H);
-        let mut res: [u8; WH] = [0; WH];
-
-        res[self.position.0] = 1;
-        res[self.position.1] = 1;
-
-        res
+    fn get_position(&self) -> (usize, usize) {
+        self.position
     }
-    
+
     fn finished(&self) -> bool {
         self.position == self.goal
     }
+
+    pub fn is_wall(&self, position: &(usize, usize)) -> bool {
+        self.maze[position.1][position.0] != 0
+    }
+
+    pub fn is_start(&self, position: &(usize, usize)) -> bool {
+        *position == self.start
+    }
+
+    pub fn is_goal(&self, position: &(usize, usize)) -> bool {
+        *position == self.goal
+    }
+
+    pub fn is_agent_position(&self, position: &(usize, usize)) -> bool {
+        *position == self.position
+    }
 }
+
 
 // const S: u8 = 2;
 // const G: u8 = 3;
@@ -112,41 +157,130 @@ impl TryFrom<usize> for Action {
     }
 }
 
+fn encode_position<const W: usize, const H: usize, const WH: usize>
+    (position: &(usize, usize)) -> [u8; WH]
+{
+    assert!(WH >= W + H);
+    let mut res: [u8; WH] = [0; WH];
+
+    res[position.0] = 1;
+    res[W + position.1] = 1;
+
+    res
+}
+fn print_maze<const W: usize, const H: usize, const WH: usize>
+    (maze: &Maze<W, H>, agent: &mut QLearning<WH, 4>)
+{
+    // let position_agent = maze.get_position();
+
+    for y in 0..H {
+        for x in 0..W {
+            print!("-------------");
+        }
+        println!("-");
+
+
+        for i in 0..3 {
+            for x in 0..W {
+                let position = (x, y);
+                let position_encoded = encode_position::<W, H, WH>(&position);
+                let (_action, q_values) = agent.choose_action(&position_encoded);
+
+                let marker = if maze.is_wall(&position) {
+                    print!("{}", BRIGHT_BLACK);
+                    '#'
+                }
+                else if maze.is_goal(&position) {
+                    'G'
+                }
+                else if maze.is_start(&position) {
+                    'S'
+                }
+                else {
+                    ' '
+                };
+
+                let marker_player = if maze.is_agent_position(&position) {
+                    '*'
+                }
+                else {
+                    ' '
+                };
+
+                match i {
+                    0 => {
+                        print!("|{}  {:5.2}    ", marker, q_values[0]);
+                    },
+                    1 => {
+                        print!("|{:5.2}{}{:5.2} ", q_values[2], marker_player, q_values[3]);
+                    },
+                    2 => {
+                        print!("|   {:5.2}    ", q_values[1]);
+                    },
+                    _ => {}
+                }
+                print!("{}", RESET);
+            }
+            println!("|");
+
+        }
+    }
+
+    for x in 0..W {
+            print!("-------------");
+    }
+    println!("-");
+    println!("");
+}
+
 #[test]
 fn test_solve_maze() {
-    const W: usize = 3;
-    const H: usize = 3;
+    const W: usize = 8;
+    const H: usize = 8;
     const WH: usize = W+H;
     let maze: [[u8; W]; H] = [
-        [0, 0, 0],
-        [1, 1, 0],
-        [0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 1, 0, 1, 1],
+        [0, 0, 0, 0, 1, 0, 1, 0],
+        [1, 0, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0, 1, 0, 0],
+        [1, 1, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
     ];
     let start = (0, 0);
-    let goal = (2, 1);
+    let goal = (0, 7);
 
     let mut maze = Maze::new(maze, start, goal);
 
 
     let mut agent: QLearning<WH, 4> = QLearning::new(); 
 
-    for _episode in 0..10 {
+    for _episode in 0..60 {
         maze.reset();
-        for _steps in 0..10 {
+        for _steps in 0..30 {
 
-            let position: [u8; WH] = maze.get_position();
+            let position = maze.get_position();
+            let position_encoded: [u8; WH] = encode_position::<W, H, WH>(&position);
 
-            let (action, _q_values) = agent.choose_action(&position);
+            let (action, _q_values) = agent.choose_action(&position_encoded);
 
             let action_ = Action::try_from(action).unwrap();
             let reward = maze.step(action_);
-            agent.set_reward(&position, action, reward);
+            let next_position = maze.get_position();
+            let next_position_encoded = encode_position::<W, H, WH>(&next_position);
+            agent.set_reward(position_encoded, action, reward, next_position_encoded);
+
+            print_maze(&maze, &mut agent);
+            std::thread::sleep(Duration::from_millis(100));
 
             if maze.finished() {
                 break;
             }
         }
         agent.learn();
+        print_maze(&maze, &mut agent);
+        // std::thread::sleep(Duration::from_secs(1));
     }
 
     assert!(maze.finished());
